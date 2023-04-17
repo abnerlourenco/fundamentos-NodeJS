36d528e70d67752a8d6f02d13cae1d2c7e30eb18
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2023-04-17 15:20:05 -0300

    feat: add delete and patch status todo

--- desafio1-conceitos-do-nodejs/src/index.js
+++ desafio1-conceitos-do-nodejs/src/index.js
@@ -80,11 +80,35 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found'});
+  }
+
+  todo.done = true;
+
+  return response.status(201).send().json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found'});
+  }
+
+  const indexTodo = user.todos.indexOf(todo);
+
+  user.todos.splice(indexTodo, 1);
+
+  return response.status(200);
 });
 
 module.exports = app;
