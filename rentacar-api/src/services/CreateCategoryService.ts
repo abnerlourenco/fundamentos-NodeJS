c22d9d981222323210d6267eb25598b1bea3ad13
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2023-05-22 16:28:18 -0300

    feat: add services layer to execute business rule

---
+++ rentacar-api/src/services/CreateCategoryService.ts
@@ -0,0 +1,22 @@
+import { type CategoriesRepository } from '../repositories/CategoriesRopository'
+
+interface IRequest {
+  name: string
+  description: string
+}
+
+class CreateCategoryService {
+  constructor (private readonly categoriesRepository: CategoriesRepository) {}
+
+  execute ({ name, description }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name)
+
+    if (categoryAlreadyExists != null) {
+      throw new Error('Category already exists')
+    }
+
+    this.categoriesRepository.create({ name, description })
+  }
+}
+
+export { CreateCategoryService }
