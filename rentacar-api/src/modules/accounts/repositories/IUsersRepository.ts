3c0b822dd7f543e095eb6b0d0f9a4cfd24b52dd9
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2023-08-24 09:11:41 -0300

    separating the projects

--- rentacar-api/src/modules/accounts/repositories/IUsersRepository.ts
+++
@@ -1,7 +0,0 @@
-import { type ICreateUserDTO } from '../dtos/ICreateUserDTO';
-
-interface IUsersRepository {
-  create: (data: ICreateUserDTO) => Promise<void>
-}
-
-export type { IUsersRepository };
