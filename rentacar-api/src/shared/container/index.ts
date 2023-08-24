f2d3e5a7377ccb6c10d039e6687641abbf4a55ea
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2023-08-23 21:59:05 -0300

    feat: add create user route and database

--- rentacar-api/src/shared/container/index.ts
+++ rentacar-api/src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
+import { type IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 import { type ICategoryRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
 import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
@@ -13,4 +15,9 @@ container.registerSingleton<ICategoryRepository>(
 container.registerSingleton<ISpecificationRepository>(
   'SpecificationRepository',
   SpecificationsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository
 );
